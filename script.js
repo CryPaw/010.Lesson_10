c5bb35d20996c1fd14bc2ba7aae387480f6411c2
Nick <119486388+CryPaw@users.noreply.github.com>
2023-02-23 15:33:09 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let start = prompt();
-    let result = [];
-    while (start >= 0) {
-      result.push(start--);
-      result.sort((a, b) => a - b);
-    }
-    result.splice(2, 3);
-    alert('[ ' + result + ' ]');
